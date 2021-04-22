026beaf17b56071c07c5670f7d954662825be99f
yhansol145 <yhansol145@gmail.com>
2021-04-22 11:07:17 +0900

    DB connect

---
+++ src/multichat/DB.java
@@ -0,0 +1,27 @@
+package multichat;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+
+	public static void main(String[] args) {
+		try {
+
+			Class.forName("oracle.jdbc.OracleDriver");
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String id = "kosmo";
+			String pass = "1234";
+
+			Connection con = DriverManager.getConnection(url, id, pass);
+			if (con != null) {
+				System.out.println("Oracle 연결성공");
+			} else {
+				System.out.println("Oracle 연결실패");
+			}
+		} catch (Exception e) {
+			System.out.println("Oracle 연결시 예외발생");
+			e.printStackTrace();
+		}
+	}
+}
